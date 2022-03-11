1904ef486f16ab506dc0a2f78c26113e40a2fd11
Johnh <johnhennagir@gmail.com>
2022-03-10 21:46:13 -0600

    committing first enum file.

---
+++ enums/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+enum week {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+
+int main()
+{
+    enum week day;
+    day = Thursday;
+    printf("%d", day);
+    return 0;
+}
+
+
+
+

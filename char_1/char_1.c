885dcde8d4ae575b356d9dda96dc644c02d402d2
Squakems <johnhennagir@gmail.com>
2022-03-14 21:09:11 -0500

    adding char_1

---
+++ char_1/char_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main ()
+
+{
+
+    char one;
+    char two;
+    char three;
+    char four;
+    char five;
+    
+    one = 'L';
+    two = 'E';
+    three = 'A';
+    four = 'R';
+    five = 'N';
+
+
+    printf("%c\n", one);
+    printf("%c\n", two);
+    printf("%c\n", three);
+    printf("%c\n", four);
+    printf("%c\n", five);
+    return 0;
+
+
+}

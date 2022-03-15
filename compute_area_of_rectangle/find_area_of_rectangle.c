df9d489e348f9b1e6614751c14692ed5ab660a63
Squakems <johnhennagir@gmail.com>
2022-03-14 21:34:52 -0500

    figuring area of triangle

---
+++ compute_area_of_rectangle/find_area_of_rectangle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main ()
+
+{
+    int height;
+    int base;
+    
+    printf("enter the triangle's height:");
+    scanf("%d", &height);
+
+    printf("enter the triangle's width:");
+    scanf("%d", &base);
+
+    printf("The Area is:%d\n", height * base / 2);    
+    return 0;
+
+
+
+
+
+
+
+}

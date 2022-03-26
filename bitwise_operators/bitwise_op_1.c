1c07c85ced39b41832d07834d064f1ffbed47789
Squakems <johnhennagir@gmail.com>
2022-03-25 21:23:49 -0500

    adding notes to bitwise ops

--- bitwise_operators/bitwise_op_1.c
+++ bitwise_operators/bitwise_op_1.c
@@ -5,9 +5,11 @@ int main ()
     unsigned int a = 60; // 0011 1100
     unsigned int b = 13; // 0000 1101
     int result = 0;
-
+    // the single & is the bitwise operator. 
     result = a & b;
 
     printf("result is %d", result);
     return 0;
 }
+
+// Don't really understand this topic yet. Need to get more time with it. 
